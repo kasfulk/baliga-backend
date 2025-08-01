78f0cfcacd838ea56210ed282a92f250db7d461d
Kasjful Kurniawan <kasful.bpp@gmail.com>
2025-08-02 00:26:14 +0800

    fix: fixing issue on config

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,15 @@
 import 'dotenv/config';
+import { Sequelize } from 'sequelize';
 
-export default {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-  },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-  },
+const env = process.env.NODE_ENV || 'development';
+const config = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
 };
+
+const sequelize = new Sequelize({ ...config, ...config[env] });
+
+export default sequelize;
